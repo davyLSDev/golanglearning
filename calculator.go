64a008fd6cb2f525fa15b9ef25ce7225962d0a77
Davy Tenn <davy.lsdev@gmail.com>
2019-10-04 13:40:10 -0600

    Begin to make improvements on processing parameters

--- calculator.go
+++ calculator.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-// a great way to remember to import a package before using it       _ "fmt"
+	// a great way to remember to import a package before using it       _ "fmt"
 	"fmt"
 	"os"
 )
@@ -9,23 +9,36 @@ import (
 // names for operands here: https://math.stackexchange.com/questions/975541/what-are-the-formal-names-of-operands-and-results-for-basic-operations
 
 func main() {
-	argCount := len(os.Args[1:])
-	operation := os.Args[2]
-	fmt.Println("There are ", argCount, "arguments entered")
-	fmt.Println("This is a very simplisitic way to do command parsing")
-	fmt.Println("You want to do the following arithmetic operation: ", operation)
-	fmt.Println("For integer arithmetic")
-	fmt.Println("Adding function 1 + 2 is: ", add(1,2))
-	fmt.Println("Subtracting function 1 - 2 is: ", sub(1,2))
-	fmt.Println("Multiplying function 1 * 2 is: ", mul(1,2))
-	fmt.Println("Dividing function 1 / 2 is: ", div(1,2))
-	fmt.Println("********************************************")
+	switch argCount := len(os.Args[1:]); argCount {
+	case 0:
+		fmt.Println("No parameters given, assuming interractive mode")
+		fmt.Println("For now check out sub 4 5, which results in: ", sub(4, 5))
+	case 1:
+		fmt.Println("Assume interractive calculator then.")
+		fmt.Println("For testing purposes, add 1 2, which results in: ", add(1, 2))
+	case 2:
+		fmt.Println("The calculator needs another operand, enter that now please.")
+	case 3:
+		fmt.Println("Much better, now we need to check for parameter sanity!")
+		operation := os.Args[2]
+
+		fmt.Println("There are ", argCount, "arguments entered")
+		fmt.Println("This is a very simplisitic way to do command parsing")
+		fmt.Println("You want to do the following arithmetic operation: ", operation)
+		fmt.Println("For integer arithmetic")
+		fmt.Println("Adding function 1 + 2 is: ", add(1, 2))
+		fmt.Println("Subtracting function 1 - 2 is: ", sub(1, 2))
+		fmt.Println("Multiplying function 1 * 2 is: ", mul(1, 2))
+		fmt.Println("Dividing function 1 / 2 is: ", div(1, 2))
+		fmt.Println("********************************************")
+	default:
+		fmt.Println("Probably best to give instructions on how to use this")
+	}
 }
 
 func add(augend, addend int) int {
-    return augend + addend
+	return augend + addend
 }
-
 
 func sub(minuend, subtrahend int) int {
 	return minuend - subtrahend
--- improvements.md
+++ improvements.md
@@ -1,15 +1,25 @@
 # Bug list
 
 ## Bugs found
-B-1. 4-Oct-2019. Without enough command line parameters, program crashes
+* **B-1.** 4-Oct-2019. Without enough command line parameters, program crashes
+* **B-2.** 4-Oct-2019. Note difference between entering "./calculator 1 2 3 4 5", and "./calculator a;lksdjflk ;aklsjdf ;laksjdf ;aksdjf ;adfj". The first yeilds the default from the switch statement, whereas the second yeilds -
+> Assume interractive calculator then.
+For testing purposes, add 1 2, which results in:  3
+lksdjflk: command not found
+aklsjdf: command not found
+laksjdf: command not found
+aksdjf: command not found
+adfj: command not found
+
 
 ## Error Handling that should be done
-EH-1. avoid division by zero
-EH-2. avoid overflowing the result
+* **EH-1.** avoid division by zero
+* **EH-2.** avoid overflowing the result
 
 ## Improvements needed
-I-1. make this work with float64 instead of integer
-I-2. get operands from the command line to do the calculation
-I-3. get command line parameters to select which calculation to do
-I-4. refactor to make a library
-I-5. add a gui to make a four banger calculator
+* **I-1.** make this work with float64 instead of integer
+* **I-2.** get operands from the command line to do the calculation
+* **I-3.** get command line parameters to select which calculation to do
+* **I-4.** refactor to make a library
+* **I-5.** add a gui to make a four banger calculator
+* **I-6** make the interractive calculator portion work
