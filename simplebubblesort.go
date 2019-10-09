88a02f95b3ab77f30dd1641d217440c2a27ba890
Davy Tenn <davy.lsdev@gmail.com>
2019-10-08 21:30:50 -0600

    Fill a slice with random numbers

--- simplebubblesort.go
+++ simplebubblesort.go
@@ -4,16 +4,24 @@ package main
 
 import (
 	"fmt"
-	"math/rand"	// to generate random numbers
-	"time" 			// to generate a random seed
+	"math/rand" // to generate random numbers
+	"time"      // to generate a random seed
 )
 
-func main () {
-	var max, min int
+//func sort
+
+func main() {
+	var max, min, numberOfNumbers int
+	numberOfNumbers = 10
+	numsToSortSlice := make([]int, numberOfNumbers)
+	const maxItterations = 10 - 1 // array size minus 1
 	max = 1000
-	min =1
+	min = 1
 	rand.Seed(time.Now().UTC().UnixNano())
-	fmt.Println ("The random number is", (rand.Intn(max - min) + min))
+	for index := 0; index < numberOfNumbers; index++ {
+		numsToSortSlice[index] = (rand.Intn(max-min) + min)
+	}
+	fmt.Println("Slice before sorting is \n", numsToSortSlice)
 }
 
 /* Ref: https://stackoverflow.com/questions/12321133/golang-random-number-generator-how-to-seed-properly
