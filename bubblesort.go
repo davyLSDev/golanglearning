aa95e686007c61bab94709882c1b4b3810415952
Davy Tenn <davy.lsdev@gmail.com>
2019-10-09 22:44:59 -0600

    Optimize the bubble sort
    If no swaps are needed on a given iteration, the job is done, stop iterating.

--- bubblesort.go
+++ bubblesort.go
@@ -1,4 +1,6 @@
-// bubblesort worst case scenario needs n-1 iterations to sort numbers
+/* bubblesort worst case scenario needs n-1 iterations to sort numbers
+The sort can be sped up if it completes before n-1 itterations by
+stopping the outer loop */
 
 package main
 
@@ -22,20 +24,27 @@ func randomFill(sliceSize int) []int {
 }
 
 func simpleSort(sortedNumbers []int) []int {
-	var temp int
+	var temp, itterationCount, swapsNeeded int
 	maxItterations := len(sortedNumbers) - 1 // worst case scenario
 	numbersToSort := maxItterations
+	swapsNeeded = 0
 
 	for itterations := 0; itterations < maxItterations; itterations++ {
+		itterationCount++
 		for index := 0; index < numbersToSort; index++ {
 			if sortedNumbers[index] > sortedNumbers[index+1] {
 				temp = sortedNumbers[index]
 				sortedNumbers[index] = sortedNumbers[index+1]
 				sortedNumbers[index+1] = temp
+				swapsNeeded++
 			}
 		}
+		if swapsNeeded == 0 {
+			break
+		}
+		swapsNeeded = 0 // reset the swap count
 	}
-
+	fmt.Println("The number of itterations needed was ", itterationCount)
 	return sortedNumbers
 }
 
