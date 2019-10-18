80dda8690559a082bed3ded35f384d9b4d697788
Davy Tenn <davy.lsdev@gmail.com>
2019-10-18 16:54:46 -0600

    Add the framework for a function

--- binarysearch.go
+++ binarysearch.go
@@ -8,7 +8,16 @@ import (
 	"fmt"
 )
 
+// having trouble recalling syntax of a function!
+// ahhh yes, don't put a comma after "input" and before "[]int"
+func binarySearch(input []int, integerToFind int) (index int) {
+	index = integerToFind
+	return
+}
+
 func main() {
+	var intToFind int
 	sorted := []int{10, 12, 33, 54, 65, 96, 107} // I had trouble remembering this syntax!
 	fmt.Println(sorted)
+	fmt.Println("The index of the number being searched for, is ", binarySearch(sorted, intToFind))
 }
