9ada92fffa4421c8fc142235f345616148196c4f
Davy Tenn <davy.lsdev@gmail.com>
2019-10-18 16:44:07 -0600

    Begin writing a binary search

---
+++ binarysearch.go
@@ -0,0 +1,14 @@
+// My understanding of this is that the algorithm has a sorted slice as the input
+// and returns the index of the value that is being searched for
+// see https://www.codementor.io/learn-programming/3-essential-algorithm-examples-you-should-know
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sorted := []int{10, 12, 33, 54, 65, 96, 107} // I had trouble remembering this syntax!
+	fmt.Println(sorted)
+}
