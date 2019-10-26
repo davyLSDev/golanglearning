fe4e28da0108a0a97729db9c3160c55fe160db13
Davy Tenn <davy.lsdev@gmail.com>
2019-10-25 20:18:50 -0600

    Make the display function accept parameters
    for placement, and character

--- ttt.go
+++ ttt.go
@@ -18,38 +18,44 @@ func restart() {
 	fmt.Println("Do this first")
 }
 
-// var setup = doFirst()
-
 func main() {
 	fmt.Println("now we are in main\n")
 	fmt.Println("Now we will restart the game")
 	restart()
-	display()
+	display(4, ('Z'))
+	display(5, ('A'))
 }
 
-func display() {
+func display(placement int, character byte) {
 	clear := "\033[2J"
-	x := byte('X')
-	y := byte('Y')
+	// x := byte('X')
+	// y := byte('Y')
 	row := 25
 	column := 1
 	message := []byte(`
-  |   |          7 | 8 | 9
+  |   |          1 | 2 | 3
 ---------        ---------
   |   |          4 | 5 | 6
 ---------        ---------
-  |   |          1 | 2 | 3
+  |   |          7 | 8 | 9
 
 Your turn, choose a number to place your X.`)
-	message[1] = 65
-	message[5] = 66
-	message[9] = 67
-	message[55] = 68
-	message[59] = ('H')
-	message[63] = ('I')
-	message[109] = ('J')
-	message[113] = x
-	message[117] = y
+
+	// index is the array to place X's and O's into the grid
+	// remember the index starts at 0
+	index := [9]int{1, 5, 9, 55, 59, 63, 109, 113, 117}
+
+	// message[index[0]] = x
+	// message[index[1]] = y
+	message[index[placement]] = character
+	// message[5] = 66
+	// message[9] = 67
+	// message[55] = 68
+	// message[59] = ('H')
+	// message[63] = ('I')
+	// message[109] = ('J')
+	// message[113] = x
+	// message[117] = y
 	output := strings.Join([]string{"\033[" + strconv.Itoa(row) + ";" + strconv.Itoa(column) + "H" + string(message)}, "+ ")
 	fmt.Println(clear, output)
 }
