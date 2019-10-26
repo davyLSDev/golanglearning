24e21681069d2844855bc9c4db6b93bd1711b291
Davy Tenn <davy.lsdev@gmail.com>
2019-10-25 18:45:42 -0600

    Begin to use byte array
    string is immutable in golang, whereas byte is not. To change values for "X", and "O", the storage area to display needs to be able to be indexed and changed.

--- ttt.go
+++ ttt.go
@@ -8,7 +8,22 @@ import (
 	"strings"
 )
 
+func init() {
+	restart()
+	// fmt.Println("Do this first")
+	//	return 1
+}
+
+func restart() {
+	fmt.Println("Do this first")
+}
+
+// var setup = doFirst()
+
 func main() {
+	fmt.Println("now we are in main\n")
+	fmt.Println("Now we will restart the game")
+	restart()
 	display()
 }
 
@@ -16,7 +31,7 @@ func display() {
 	clear := "\033[2J"
 	row := 25
 	column := 1
-	message := `
+	message := []byte(`
   |   |          7 | 8 | 9
 ---------        ---------
   |   |          4 | 5 | 6
@@ -24,7 +39,8 @@ func display() {
   |   |          1 | 2 | 3
 
 Your turn, choose a number to place your X.
-	`
-	output := strings.Join([]string{"\033[" + strconv.Itoa(row) + ";" + strconv.Itoa(column) + "H" + message}, "+ ")
+	`)
+
+	output := strings.Join([]string{"\033[" + strconv.Itoa(row) + ";" + strconv.Itoa(column) + "H" + string(message)}, "+ ")
 	fmt.Println(clear, output)
 }
