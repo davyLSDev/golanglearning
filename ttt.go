1c55380b25e313a9107661f07cf971c45cd1267b
Davy Tenn <davy.lsdev@gmail.com>
2019-10-25 20:52:39 -0600

    Get players input and place X on board
    Also, keep the loop going

--- ttt.go
+++ ttt.go
@@ -8,25 +8,32 @@ import (
 	"strings"
 )
 
-func init() {
-	restart()
-	// fmt.Println("Do this first")
-	//	return 1
-}
+// func init() {
+// 	restart()
+// 	// fmt.Println("Do this first")
+// 	//	return 1
+// }
 
-func restart() {
-	fmt.Println("Do this first")
-}
+// func restart() {
+// 	fmt.Println("Do this first")
+// }
 
 func main() {
-	fmt.Println("now we are in main\n")
-	fmt.Println("Now we will restart the game")
-	restart()
-	display(4, ('Z'))
-	display(5, ('A'))
+	// Wait to implement restart()
+	display(0, (' '))
+	playerMark := byte('X')
+	// robotMark := byte('Y')
+	var location int32
+
+	for {
+		_, err := fmt.Scanf("%d", &location)
+		fmt.Println(err)
+		mark := playerMark
+		display(location-1, mark)
+	}
 }
 
-func display(placement int, character byte) {
+func display(placement int32, character byte) {
 	clear := "\033[2J"
 	// x := byte('X')
 	// y := byte('Y')
@@ -39,7 +46,7 @@ func display(placement int, character byte) {
 ---------        ---------
   |   |          7 | 8 | 9
 
-Your turn, choose a number to place your X.`)
+Your turn, choose a number to place your X, and press <ENTER>`)
 
 	// index is the array to place X's and O's into the grid
 	// remember the index starts at 0
