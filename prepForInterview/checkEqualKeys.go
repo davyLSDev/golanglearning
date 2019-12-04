4504b9d43ae24021f9915cd8407ed78d7d6b5126
Davy <davy.lsdev@gmail.com>
2019-12-03 23:24:33 -0700

    Check equal keystroke sequences initial commit

---
+++ prepForInterview/checkEqualKeys.go
@@ -0,0 +1,44 @@
+// checkEquivalentKeypresses is a function that takes an array containing
+// two strings where each string represents keypresses separated by commas.
+// A keypress can be either a printable character or a backspace 
+// (represented by -B).
+// The function determines if the two strings of keypresses are equivalent.
+
+// Produce a printable string from such a string of keypresses by having
+// backspaces erase one preceding character. Consider two strings of
+// keypresses equivalent if they produce the same printable string.
+
+// Example datasets:
+
+// checkEquivalentKeypresses(["a,b,c,d", "a,b,c,c,-B,d"]) // true
+// checkEquivalentKeypresses(["-B,-B,-B,c,c", "c,c"]) // true
+// checkEquivalentKeypresses(["", "a,-B,-B,a,-B,a,b,c,c,c,d"]) // false
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	datasets:= [][]string{
+		{"a,b,c,d", "a,b,c,c,-B,d"},
+		{"-B,-B,-B,c,c", "c,c"},
+		{"", "a,-B,-B,a,-B,a,b,c,c,c,d"},
+	}
+	
+	for _, dataset := range datasets {
+		test := checkEquivalentKeypresses(dataset)
+		if test {
+			fmt.Println("The two strings are equivalent")
+		} else {
+			fmt.Println("The two strings are not equivalent")
+		}
+	}
+}
+
+func checkEquivalentKeypresses(stringsToTest []string) (status bool) {
+	fmt.Println("Strings to test are ", stringsToTest)
+	status = true
+	return status
+}
