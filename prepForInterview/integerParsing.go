6287c1b5ffff1fe5760dc7d4ea519b0b59a1ccdb
Davy Tenn <davy.lsdev@gmail.com>
2020-02-06 11:55:10 -0700

    Compare integer parsing with string parsing

--- prepForInterview/integerParsing.go
+++ prepForInterview/integerParsing.go
@@ -1,22 +1,13 @@
 package main
 
-
-
-
-
-
-
-
-
-
-
-
+// The clear winner is integer parsing vs string converted based parsing
 
 // how to scan a line of text from keyboard https://stackoverflow.com/questions/20895552/how-to-read-from-standard-input-in-the-console
 // more useful: https://stackoverflow.com/questions/3751429/reading-an-integer-from-standard-input
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -31,7 +22,13 @@ func main() {
 	t0 := time.Now()
 	parsedIntegerNumber = integerParsing(integer)
 	t1 := time.Now()
-	fmt.Println("Time for string converted integer parsing ", t1.Sub(t0))
+	fmt.Println("Time for arithmetic integer parsing ", t1.Sub(t0))
+	fmt.Println("This is the parsed integer ", parsedIntegerNumber)
+
+	t2 := time.Now()
+	parsedIntegerNumber = integerToStringParsing(integer)
+	t3 := time.Now()
+	fmt.Println("Time for string converted integer parsing ", t3.Sub(t2))
 	fmt.Println("This is the parsed integer ", parsedIntegerNumber)
 }
 
@@ -43,6 +40,16 @@ func integerParsing(number int) (parsedNumber []int) {
 		digit = number % 10
 		parsedNumber = append([]int{digit}, parsedNumber...)
 		number = number/10
+	}
+	return parsedNumber
+}
+func integerToStringParsing(number int) (parsedNumber []int) {
+	var numberString string
+	var convertedNumber int
+	numberString = strconv.Itoa(number)
+	for number := range numberString {
+		convertedNumber, _ = strconv.Atoi(string(numberString[number]))
+		parsedNumber = append(parsedNumber, convertedNumber)
 	}
 	return parsedNumber
 }
