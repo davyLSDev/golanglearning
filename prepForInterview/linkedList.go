19e7e368244f319665c7bd7ec2eb0235c36352f6
Davy Tenn <davy.lsdev@gmail.com>
2020-03-10 16:41:54 -0600

    Create the skeleton
    Make prompts and use Fetch to get user input

--- prepForInterview/linkedList.go
+++ prepForInterview/linkedList.go
@@ -1,9 +1,51 @@
 package main
 
+/* Simple linked list which allows for
+(getting input from the user)
+1. adding new nodes to the list
+2. print out the list of nodes like HEAD: 12 -> 33 -> TAIL: 44
+*/
+
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+const CLEAR = "\033[2J"
+const PROMTP1 = CLEAR + `
+**************************************
+* Choose one of the following        *
+*                                    *
+* 0   DEBUG linked list printout     *
+* 1   print out the linked list      *
+* 2   insert new value to the list   *
+*                                    *
+**************************************`
+const PROMPT2 = CLEAR + `
+**************************************
+*  Enter a value to add to the list  *
+**************************************`
+
 func main() {
-	fmt.Println("Learn about linked lists")
+	var choice, value string
+	choice = Fetch(PROMTP1)
+	value = Fetch(PROMPT2)
+	fmt.Println("Your command was ", choice)
+	fmt.Println("Your value was", value)
+}
+
+func Fetch(prompt string) (lineOfCharacters string) {
+
+	fmt.Println(prompt)
+	/* Note to include whitespace when fetching input, you need to use bufio.Scanner, see
+			   https://stackoverflow.com/questions/43843477/scanln-in-golang-doesnt-accept-whitespace
+	   	   	   	fmt.Scanln(&aString)
+	*/
+	keyScanner := bufio.NewScanner(os.Stdin)
+	if keyScanner.Scan() {
+		lineOfCharacters = keyScanner.Text()
+	}
+
+	return lineOfCharacters
 }
