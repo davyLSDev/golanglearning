c79ca5b1841cf528d6bb7918ced0244bb84e1046
Davy Tenn <davy.lsdev@gmail.com>
2020-03-11 12:26:15 -0600

    Set up simple CLI menu

--- prepForInterview/linkedList.go
+++ prepForInterview/linkedList.go
@@ -10,8 +10,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"time"
 )
 
+const FINISH = true
+const AGAIN = false
+const PAUSE = 2 * time.Second
+const NOPAUSE = 0 * time.Second
 const CLEAR = "\033[2J"
 const PROMTP1 = CLEAR + `
 **************************************
@@ -19,20 +24,57 @@ const PROMTP1 = CLEAR + `
 *                                    *
 * 0   DEBUG linked list printout     *
 * 1   print out the linked list      *
-* 2   insert new value to the list   *
+* 2   add a new value to the list    *
+* 3   exit                           *
 *                                    *
 **************************************`
 const PROMPT2 = CLEAR + `
 **************************************
 *  Enter a value to add to the list  *
 **************************************`
+const ERROR1 = CLEAR + `
+**************************************
+*  Please enter a number between 0-2!*
+**************************************`
+const ACTION0 = CLEAR + "DEBUG printout of list"
+const ACTION1 = CLEAR + "Print out the linked list"
+const ACTION2 = CLEAR + "Fetch a value to add to the list"
+const ACTION3 = CLEAR + "Exit now"
 
 func main() {
-	var choice, value string
-	choice = Fetch(PROMTP1)
-	value = Fetch(PROMPT2)
-	fmt.Println("Your command was ", choice)
-	fmt.Println("Your value was", value)
+	var action, choice, value string
+	loopState := AGAIN
+	pauseTime := PAUSE //NOPAUSE is default
+
+	for {
+		choice = Fetch(PROMTP1)
+		switch choice {
+		case "0":
+			action = ACTION0
+		case "1":
+			action = ACTION1
+		case "2":
+			action = ACTION2
+			value = Fetch(PROMPT2)
+			fmt.Println(CLEAR+"Your value was", value, "it will now be added to the list")
+			time.Sleep(pauseTime)
+		case "3":
+			action = ACTION3
+			loopState = FINISH
+		default:
+			action = ERROR1
+		}
+
+		if loopState == FINISH {
+			// break
+			fmt.Println("The loop should end now")
+			time.Sleep(pauseTime)
+			break
+		}
+	}
+
+	fmt.Println(action)
+	time.Sleep(pauseTime)
 }
 
 func Fetch(prompt string) (lineOfCharacters string) {
