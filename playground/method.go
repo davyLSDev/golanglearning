95d904bb926701eb9e673eb26f638fbd95700391
Davy <davy.lsdev@gmail.com>
2020-03-19 18:47:08 -0600

    Make procedures with senders that by value and by pointer to value

--- playground/method.go
+++ playground/method.go
@@ -17,6 +17,20 @@ func (c Car) displayFeatures() {
 	fmt.Printf("This %s is made by %s and has a %s engine. \n", c.model, c.make, c.engine)
 }
 
+// must use a pointer or else changes made inside this method will then be visible to the caller
+func (c *Car) addFeatures(make, model, engine string) {
+	c.make = make
+	c.model = model
+	c.engine = engine
+}
+
+func (c Car) addFeaturesByValue(make, model, engine string) {
+	c.make = make
+	c.model = model
+	c.engine = engine
+	fmt.Println("DEBUG: ", c.make, c.model, c.engine)
+}
+
 func main() {
 	car1 := Car{
 		make:   "Toyota",
@@ -30,4 +44,14 @@ func main() {
 		engine: "4.3 litre V8",
 	}
 	car2.displayFeatures()
+
+	fmt.Println("First use a procedure with a value as the sender")
+	car3 := Car{}
+	car3.addFeaturesByValue("Mazda", "Protege", "2 litre v4")
+	car3.displayFeatures()
+
+	fmt.Println("Now compare with the procedure which uses a pointer to the value as the sender")
+
+	car3.addFeatures("Mazda", "Protege", "2 litre v4")
+	car3.displayFeatures()
 }
