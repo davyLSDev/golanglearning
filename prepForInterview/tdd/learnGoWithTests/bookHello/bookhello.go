eb05155ce02f5f0a0246c15e37196be4e2028948
Davy <davy.lsdev@gmail.com>
2020-04-08 15:50:20 -0600

    Split Hello into Hello and greetingPrefix
    No new tests had to be written as no new functionality has been added, and no functionality has been taken away.

--- prepForInterview/tdd/learnGoWithTests/bookHello/bookhello.go
+++ prepForInterview/tdd/learnGoWithTests/bookHello/bookhello.go
@@ -10,20 +10,32 @@ const spanishHelloPrefix = "Hola, "
 
 // Hello is the main function
 func Hello(name, language string) string {
-	prefix := englishHelloPrefix
+	// prefix := englishHelloPrefix
 
 	if name == "" {
 		name = "World"
 	}
 
+	// switch language {
+	// case french:
+	// 	prefix = frenchHelloPrefix
+	// case spanish:
+	// 	prefix = spanishHelloPrefix
+	// }
+
+	// return prefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	prefix = englishHelloPrefix
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
 	}
-
-	return prefix + name
+	return prefix
 }
 
 func main() {
