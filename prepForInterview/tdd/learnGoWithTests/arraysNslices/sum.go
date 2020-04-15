d721cb14d325df45d21010ec8a66f47a445dca2a
Davy <davy.lsdev@gmail.com>
2020-04-15 14:45:33 -0600

    Use a slice instead of a fixed array

--- prepForInterview/tdd/learnGoWithTests/arraysNslices/sum.go
+++ prepForInterview/tdd/learnGoWithTests/arraysNslices/sum.go
@@ -1,7 +1,7 @@
 package main
 
 // Sum takes an array and returns the sum of all the elements
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum = sum + number
