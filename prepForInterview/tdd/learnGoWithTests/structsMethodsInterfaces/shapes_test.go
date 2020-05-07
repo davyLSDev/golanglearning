b27e494f271c12ac87c7fa824fc204b575dfaae2
Davy <davy.lsdev@gmail.com>
2020-05-07 11:42:57 -0600

    Define a rectangle
    use rectangle as a shape

--- prepForInterview/tdd/learnGoWithTests/structsMethodsInterfaces/shapes_test.go
+++ prepForInterview/tdd/learnGoWithTests/structsMethodsInterfaces/shapes_test.go
@@ -5,7 +5,8 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
@@ -13,7 +14,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 12.0)
+	rectangle := Rectangle{10.0, 12.0}
+	got := Area(rectangle)
 	want := 120.0
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
