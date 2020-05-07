6a63cb79c0454597203c01faec4eb072a857e4a0
Davy <davy.lsdev@gmail.com>
2020-05-07 11:33:00 -0600

    Make a slightly better area test

--- prepForInterview/tdd/learnGoWithTests/structsMethodsInterfaces/shapes_test.go
+++ prepForInterview/tdd/learnGoWithTests/structsMethodsInterfaces/shapes_test.go
@@ -13,8 +13,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
+	got := Area(10.0, 12.0)
+	want := 120.0
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
