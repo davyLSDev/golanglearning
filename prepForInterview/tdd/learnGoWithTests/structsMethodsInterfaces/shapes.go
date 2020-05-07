10f72e31b7ff1524e3301dbebb15573e0b80c03e
Davy <davy.lsdev@gmail.com>
2020-05-07 11:59:52 -0600

    Define circle and how to calculate area

--- prepForInterview/tdd/learnGoWithTests/structsMethodsInterfaces/shapes.go
+++ prepForInterview/tdd/learnGoWithTests/structsMethodsInterfaces/shapes.go
@@ -1,8 +1,23 @@
 package main
 
+import "math"
+
+type Circle struct {
+	Radius float64
+}
+
+// I couldn't quickly determine a better way to express squaring the Radius
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
 }
 
 func Perimeter(rectangle Rectangle) float64 {
